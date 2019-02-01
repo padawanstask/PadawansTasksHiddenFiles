0c649492e17fb18ea9db9ab2d107726342f3139d
padawanstask <46050132+padawanstask@users.noreply.github.com>
2019-02-01 18:19:23 +0300

    Update HiddenTest.cs

--- PadawansTask1/HiddenTest.cs
+++ PadawansTask1/HiddenTest.cs
@@ -22,26 +22,6 @@ namespace PadawansTask1.Tests
             message:"Initial population cannot be less or equals zero.");
         }
 
-        [TestCase(1500, 101, 100, 5000)]
-        [TestCase(1500, -1, 100, 5000)]
-        [Property("Mark", 2)]
-        public void GetYearsTest_PercentOutOfRange_ThrowArgumentOutOfRangeException(int initialPopulation, double percent, int visitors, int currentPopulation)
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() =>
-                    Population.GetYears(initialPopulation, percent, visitors, currentPopulation),
-                message: "Value of percents cannot be less then 0% or more then 100%.");
-        }
-
-        [TestCase(1500, 5, -100, 5000)]
-        [TestCase(1500, 1, -5, 5000)]
-        [Property("Mark", 2)]
-        public void GetYearsTest_Visitors_LessOrEqualsZero_ThrowArgumentException(int initialPopulation, double percent, int visitors, int currentPopulation)
-        {
-            Assert.Throws<ArgumentException>(() =>
-                    Population.GetYears(initialPopulation, percent, visitors, currentPopulation),
-                message: "Count of visitors cannot be less zero.");
-        }
-
         [TestCase(0, 0.25, 1000, 0)]
         [TestCase(-100, 0.25, 1000, -100)]
         [Property("Mark", 2)]
