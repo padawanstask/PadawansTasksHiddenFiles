709e3ab7b9d5c41a69aa66dfb6fef9f24c60fb59
padawanstask <46050132+padawanstask@users.noreply.github.com>
2019-02-05 19:27:30 +0300

    Update HiddenTest.cs

--- PadawansTask6/HiddenTest.cs
+++ PadawansTask6/HiddenTest.cs
@@ -12,22 +12,18 @@ namespace PadawansTask6.Tests
         [TestCase(414, ExpectedResult = 441)]
         [TestCase(144, ExpectedResult = 414)]
         [TestCase(1234321, ExpectedResult = 1241233)]
-        [TestCase(1234126, ExpectedResult = 1234162)]
         [TestCase(3456432, ExpectedResult = 3462345)]
         [TestCase(124121133, ExpectedResult = 124121313)]
         public int? NextBiggerThan_WithNumberForWhichBiggerNumberExists(int number) 
             => NumberFinder.NextBiggerThan(number);
 
-        [TestCase(10, ExpectedResult = null)]
         [TestCase(int.MaxValue, ExpectedResult = null)]
-        [TestCase(2, ExpectedResult = null)]
         [TestCase(2000, ExpectedResult = null)]
         [TestCase(111111111, ExpectedResult = null)]
         public int? NextBiggerThan_WithNumberForWhichBiggerNumberDoesNotExist(int number) 
             => NumberFinder.NextBiggerThan(number);
 
         [TestCase(-1)]
-        [TestCase(-10)]
         [TestCase(int.MinValue)]
         public void NextBiggerThan_WithNegativeNumber_ThrowArgumentException(int number)
             => Assert.Throws<ArgumentException>(() => NumberFinder.NextBiggerThan(number),
